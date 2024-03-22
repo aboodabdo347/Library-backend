fe688f1737c9e6c16c5edc666b3ca50158508eec
Ali Albanna <ali@albanna.com>
2024-03-22 14:51:12 +0300

    db connection setup ready

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express")
 const app = express();
+const logger = require('morgan');
+require('dotenv').config();
+require('./config/database');
+
+//routers go here
 
 app.listen(3000, () => {
     console.log("listening on port 3000")
