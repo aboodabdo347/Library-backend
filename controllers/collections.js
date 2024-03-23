7b2a587373478b322749b9088e99b6bdec3589e4
Ali Albanna <ali@albanna.com>
2024-03-24 01:19:18 +0300

    collection controller & routes

---
+++ controllers/collections.js
@@ -0,0 +1,34 @@
+const Collection = require("../models/Collection");
+
+const allCollections = async (req, res) => {
+    try {
+        let allRes = await Collection.find();
+        res.json(allRes);
+    } catch (error) {
+        res.json({error: error.message})
+    }
+}
+
+const userCollections = async (req, res) => {
+    try {
+        let userCollRes = await Collection.find({_id: res.params.id})
+        res.json(userCollRes)
+    } catch (error) {
+        res.json({error: error.message})
+    }
+}
+
+const createCollection = async (req, res) => {
+    try {
+        let collCreateRes = await Collection.create(req.body)
+        res.json(collCreateRes)
+    } catch (error) {
+        res.json({error: error.message})
+    }
+}
+
+module.exports = {
+    allCollections,
+    userCollections,
+    createCollection
+}
