27911e1ec2ea6fedd0bbe0f3bcc1fd7e188e37ee
aboodabdo347 <aboodabdo347@gmail.com>
2024-03-23 04:37:14 +0300

    in the Auth

--- seed.js
+++ seed.js
@@ -1,14 +1,16 @@
-const axios = require("axios")
+const axios = require('axios')
 
 const getBooks = async () => {
-    const bookRes = await axios.get("https://www.googleapis.com/books/v1/volumes?q=48laws&langRestrict=en")
-    // console.log(bookRes.data.items)
+  const bookRes = await axios.get(
+    'https://www.googleapis.com/books/v1/volumes?q=48laws&langRestrict=en'
+  )
+  // console.log(bookRes.data.items)
 
-    for(let i = 0; i < 20; i++) {
-        if(bookRes.data.items[i]) {
-            console.log(bookRes.data.items[i].volumeInfo.title);
-            console.log(bookRes.data.items[i].volumeInfo.authors)
-        }
+  for (let i = 0; i < 20; i++) {
+    if (bookRes.data.items[i]) {
+      console.log(bookRes.data.items[i].volumeInfo.title)
+      console.log(bookRes.data.items[i].volumeInfo.authors)
     }
+  }
 }
-getBooks();
+getBooks()
