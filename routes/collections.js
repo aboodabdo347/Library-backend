0f54dc7ce4eeccfc977cc6db9ba8198e4b58d2d3
Ali Albanna <ali@albanna.com>
2024-03-26 22:01:18 +0300

    fix auth user data

--- routes/collections.js
+++ routes/collections.js
@@ -2,8 +2,9 @@ const express = require("express")
 const router = express.Router()
 const collectionCTRL = require("../controllers/collections")
 
-router.get("/", collectionCTRL.allCollections)
 router.get("/:id", collectionCTRL.userCollections)
+
+router.get("/", collectionCTRL.allCollections)
 
 router.post("/", collectionCTRL.createCollection)
 
